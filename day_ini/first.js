068a8e062c05edb8b83c4204954a43817fc12003
dollyraghav22 <dollyraghav64@gmail.com>
2026-08-19 23:46:29 +0530

    string

--- day_ini/first.js
+++ day_ini/first.js
@@ -1,25 +1,7 @@
-// let number = prompt("enter the number");
-// console.log(number);
-// if (number%5==0) {
-//     console.log("yes");
-// }
-// else {
-//     console.log("no");
-// }
-let score = prompt("enter the score");
-console.log(score);
-if (score>=80 && score<=100){
-    console.log("Grade A");
-}
-else if (score>=70 && score<=89) {
-    console.log("Grade B");
-}
-else if (score>=60 && score<=69) {
-    console.log("Grade C");
-}
-else if (score>=50 && score<=59) {
-    console.log("Grade D");
-}
-else{
-    console.log("Grade F");
-}
+//strings in JS
+let str = "DOLLY RAGHAV";
+let str2 = 'DOLLY RAGHAV';
+console.log(str2[6]);
+let splstr = `This is a template literal`;
+console.log(splstr);
+
