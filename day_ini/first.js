3e26f8cdc6d7baedf67dc17f46744cc394f68025
dollyraghav22 <dollyraghav64@gmail.com>
2026-08-23 01:14:42 +0530

    Question_filter

--- day_ini/first.js
+++ day_ini/first.js
@@ -1,7 +1,7 @@
-//FILTER IN JS :)
-let arr = [1,2,3,2];
+//QUESTION USING FILTER IN JS :)
+let arr = [91,45,32,100];
 let newarray = arr.filter((val)=>{
-    return (val%2==0);
+    return (val>90);
 });
 console.log(newarray);
 
